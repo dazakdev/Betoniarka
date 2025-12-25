94bee09f732c9637a89412f5356dfdea0fbcd618
lythx <lythxxx@gmail.com>
2025-12-25 14:22:45 +0100

    add minimal category endpoints for testing

---
+++ src/main/java/com/betoniarka/biblioteka/category/CategoryController.java
@@ -0,0 +1,33 @@
+package com.betoniarka.biblioteka.category;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// TODO service etc.
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryRepository repository;
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@Valid @RequestBody Category category) {
+        return repository.save(category);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
